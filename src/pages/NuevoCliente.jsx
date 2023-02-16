3feccc54c99369ec7a095cd7b5843f05d6874331
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-16 17:35:46 -0600

    Creando un action para el Formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,17 +1,29 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Form } from 'react-router-dom'
 import Formulario from '../components/Formulario'
+
+/* export function action(){
+  
+} */
+
+export const action = () => {
+  console.log('Submit al formulario...')
+  return { ok: true };
+}
 
 export const NuevoCliente = () => {
 
   const navigate = useNavigate()
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900"> Nuveo Cliente</h1>
       <p className="mt-3"> Llena todos los campos para registrar un nuevo cliente</p>
 
       <div className=" flex justify-end">
+
         <button className=" bg-blue-800 text-white px-3 py-1 font-bold uppercase"
-          onClick={() => navigate(-1)}>
+          onClick={() => navigate(-1)}
+          >
           Volver
         </button>
       </div>
@@ -19,15 +31,17 @@ export const NuevoCliente = () => {
 
       <div className=' bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
 
-        <form >
+        <Form
+          method='post'
+        >
           <Formulario />
           <input
             type='submit'
             className=' mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
             value="Registrar Cliente"
           />
-        </form>
+        </Form>
       </div>
-      </>
-      )
+    </>
+  )
 } 
