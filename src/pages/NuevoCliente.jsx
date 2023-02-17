c36ed2cecf7646a34c4f88c5f919dea30448927e
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-02-16 19:37:57 -0600

    Introducción a JSON-Server

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -6,12 +6,20 @@ import { Error } from '../components/Error'
 export const action = async ({ request }) => {
   const formData = await request.formData()
   const datos = Object.fromEntries(formData)
+  const email = formData.get('email')
 
   //Validacion
   const errores = []
   if (Object.values(datos).includes('')) {
     errores.push('Todos los campos son obligatorios ')
   }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+  if(!regex.test(email)){
+    errores.push('El Email no es Valido')
+  }
+
 
   // Retornar datos si hay errores
   if (Object.keys(errores).length) {
@@ -49,6 +57,7 @@ export const NuevoCliente = () => {
 
         <Form
           method='post'
+          noValidate
         >
           <Formulario />
           <input
